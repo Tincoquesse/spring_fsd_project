0d3d49f77f8b13ac0d169a12740c0f9e62da8842
Tincoquesse <kamil.sound@gmail.com>
2022-05-28 09:11:06 +0200

    Initial commit

---
+++ src/main/java/com/inqoo/spring_fsd_project/SpringFsdProjectApplication.java
@@ -0,0 +1,13 @@
+package com.inqoo.spring_fsd_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFsdProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFsdProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inqoo/spring_fsd_project/SpringFsdProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inqoo.spring_fsd_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFsdProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
