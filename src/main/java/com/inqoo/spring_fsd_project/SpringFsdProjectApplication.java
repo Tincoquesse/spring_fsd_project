c1ae2c5569cd34e50a16679d2c331fb7af5e4824
Tincoquesse <kamil.sound@gmail.com>
2022-05-28 13:52:19 +0200

    init wired

--- src/main/java/com/inqoo/spring_fsd_project/SpringFsdProjectApplication.java
+++ src/main/java/com/inqoo/spring_fsd_project/SpringFsdProjectApplication.java
@@ -1,31 +1,31 @@
 package com.inqoo.spring_fsd_project;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 
+@Import(value = MyBean5.class)
 @SpringBootApplication
 public class SpringFsdProjectApplication implements CommandLineRunner {
 
     @Autowired
-    ApplicationContext applicationContext;
+    MyBean5 myBean5;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringFsdProjectApplication.class, args);
     }
 
-    @Bean
-    MyBean5 myBean5(MyBean1 myBean1, MyBean2 myBean2, MyBean3 myBean3, MyBean4 myBean4){
-        return new MyBean5(myBean1, myBean2, myBean3, myBean4);
-    }
+//    @Bean
+//     MyBean5 myBean5(MyBean1 myBean1, MyBean2 myBean2, MyBean3 myBean3, MyBean4 myBean4){
+//        return new MyBean5(myBean1, myBean2, myBean3, myBean4);
+//    }
     @Override
     public void run(String... args) throws Exception {
 
-        MyBean5 myBean5 =applicationContext.getBean(MyBean5.class);
         myBean5.hello();
-
     }
 }
