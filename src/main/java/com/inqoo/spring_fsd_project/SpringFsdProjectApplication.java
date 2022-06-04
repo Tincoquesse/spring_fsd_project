a8485636fd3ff100d9b764c03bc781a0a8fb343f
Tincoquesse <kamil.sound@gmail.com>
2022-06-04 07:17:22 +0200

    init2

--- src/main/java/com/inqoo/spring_fsd_project/SpringFsdProjectApplication.java
+++ src/main/java/com/inqoo/spring_fsd_project/SpringFsdProjectApplication.java
@@ -5,12 +5,15 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class SpringFsdProjectApplication implements CommandLineRunner {
 
     @Autowired
     ApplicationContext applicationContext;
+    @Autowired
+    BeanNumberInterface beanNumberInterface;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringFsdProjectApplication.class, args);
@@ -19,14 +22,18 @@ public class SpringFsdProjectApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        BeanNumberInterface numberInterface = applicationContext.getBean(BeanNumberInterface.class);
-        numberInterface.addNumber(1);
-        numberInterface.addNumber(2);
-        numberInterface.addNumber(3);
-        numberInterface.addNumber(4);
-        numberInterface.addNumber(5);
-        numberInterface.addNumber(6);
-        numberInterface.getList().forEach(number -> System.out.println(number));
-
+//        BeanNumberInterface numberInterface = applicationContext.getBean(BeanNumberInterface.class);
+//        numberInterface.addNumber(1);
+//        numberInterface.addNumber(2);
+//        numberInterface.addNumber(3);
+//        numberInterface.addNumber(4);
+//        numberInterface.addNumber(5);
+//        numberInterface.addNumber(6);
+//        numberInterface.getList().forEach(number -> System.out.println(number));
+        beanNumberInterface.addNumber(1);
+        beanNumberInterface.addNumber(2);
+        beanNumberInterface.addNumber(13);
+        beanNumberInterface.addNumber(4);
+        beanNumberInterface.getList().forEach(System.out::println);
     }
 }
